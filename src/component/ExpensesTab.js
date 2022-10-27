22119b003c09f38b8c585e599c27beb0e7784c52
murilo <murilocarrijoadm@gmail.com>
2022-10-27 17:03:47 -0300

    chore: preparando projeto para github pessoal

--- src/component/ExpensesTab.js
+++ src/component/ExpensesTab.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import '../style/ExpensesTab.css';
+
 class ExpensesTab extends React.Component {
   render() {
     const { expenses } = this.props;
     console.log(expenses);
     return (
-      <section>
-        <table>
-          <tr>
+      <table className="table-contaienr">
+        <thead>
+          <tr className="table-header">
             <th>Descrição</th>
             <th>Tag</th>
             <th>Método de pagamento</th>
@@ -20,30 +22,33 @@ class ExpensesTab extends React.Component {
             <th>Moeda de conversão</th>
             <th>Editar/Excluir</th>
           </tr>
+        </thead>
+        <tbody className="table-header">
           { expenses.length > 0
-               && expenses.map((expense) => (
-                 <tr key={ expense.id }>
-                   <td>{expense.description}</td>
-                   <td>{expense.tag}</td>
-                   <td>{expense.method}</td>
-                   <td>{expense.value}</td>
-                   <td>{expense.exchangeRates[expense.currency].name}</td>
-                   <td>
-                     {
-                       Number(expense.exchangeRates[expense.currency].ask).toFixed(2)
-                     }
+                && expenses.map((expense) => (
+                  <tr key={ expense.id }>
+                    <td>{expense.description}</td>
+                    <td>{expense.tag}</td>
+                    <td>{expense.method}</td>
+                    <td>{expense.value}</td>
+                    <td>{expense.exchangeRates[expense.currency].name}</td>
+                    <td>
+                      {
+                        Number(expense.exchangeRates[expense.currency].ask).toFixed(2)
+                      }
 
-                   </td>
-                   <td>
-                     { (
-                       expense
-                         .exchangeRates[expense.currency].ask * expense.value).toFixed(2)}
-                   </td>
-                   <td>Real</td>
-                 </tr>
-               )) }
-        </table>
-      </section>
+                    </td>
+                    <td>
+                      { (
+                        expense
+                          .exchangeRates[expense.currency].ask * expense.value)
+                        .toFixed(2)}
+                    </td>
+                    <td>Real</td>
+                  </tr>
+                )) }
+        </tbody>
+      </table>
     );
   }
 }
